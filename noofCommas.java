588900d66860a082670b93ad5d6baa22210cabbe
Ausvenkat <36623888+Ausvenkat@users.noreply.github.com>
2019-08-26 10:03:21 +0530

    Update noofCommas.java

--- noofCommas.java
+++ noofCommas.java
@@ -1,19 +1,19 @@
-*******************************************************************************/
-import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		String a=s.next();
-		int b=s.nextInt();
-		for(int i=0;i<a.length();i++)
-		{
-		System.out.print(a.charAt(i));
-        if(b>0)
-        {
-		System.out.print(",");
-		b--;
-		}
-		}
-	}
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int k;
+    cin>>k;
+    for(int i=0;i<s.length();i++){
+        cout<<s[i];
+        if(k>0){
+            cout<<",";
+            k--;
+        }
+    }
+    
+    
+    return 0;
+    
 }
